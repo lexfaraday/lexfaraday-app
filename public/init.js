d0d3072afc35833db5b1c4c28437870653ee7019
lexfaraday <lexfaraday@outlook.com>
2016-07-03 17:32:10 +0200

    Remove debug FB

--- public/init.js
+++ public/init.js
@@ -7,7 +7,13 @@ window.fbAsyncInit = function () {
 };
 
 
-
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 
 (function (i, s, o, g, r, a, m) {
