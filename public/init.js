60c64d1b77fd78a577a1035c87aee8e71842ee19
lexfaraday <lexfaraday@outlook.com>
2016-07-23 11:27:51 +0200

    Add SEO keywords

--- public/init.js
+++ public/init.js
@@ -5,14 +5,14 @@ window.fbAsyncInit = function () {
         version: 'v2.6'
     });
 };
-
+/*
 (function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));*/
 
 
 (function (i, s, o, g, r, a, m) {
