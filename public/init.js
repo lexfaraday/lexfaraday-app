6151e1b5a27ecbee4b92a628c18405f33cfc82de
lexfaraday <lexfaraday@outlook.com>
2016-07-23 10:50:24 +0200

    Remove countdown

--- public/init.js
+++ public/init.js
@@ -6,7 +6,6 @@ window.fbAsyncInit = function () {
     });
 };
 
-
 (function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
@@ -31,56 +30,8 @@ window.fbAsyncInit = function () {
 ga('create', 'UA-79899252-1', 'auto');
 ga('send', 'pageview');
 
-function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
-function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    if (clock) {
-        var daysSpan = clock.querySelector('.days');
-        var hoursSpan = clock.querySelector('.hours');
-        var minutesSpan = clock.querySelector('.minutes');
-        var secondsSpan = clock.querySelector('.seconds');
-
-        function updateClock() {
-            var t = getTimeRemaining(endtime);
-
-            daysSpan.innerHTML = t.days;
-            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-            if (t.total <= 0) {
-                clearInterval(timeinterval);
-            }
-        }
-
-        updateClock();
-        var timeinterval = setInterval(updateClock, 1000);
-    }
-}
-
-
 // TODO move to typescript
 window.onload = function(){
-    var date = new Date(Date.UTC(2016, 6, 10, 12, 0, 0));
-    var now = new Date();
-    var diff = date.getTime() - now.getTime();
-    // 15 * 24 * 60 * 60 * 1000
-    var deadline = new Date(Date.parse(new Date()) + diff);
-    initializeClock('clockdiv', deadline);
-
     $('#subscribeOK').hide();
 
     $('#contactForm').submit(function () {
